3b37e50660dc2455324f652c39d750652a7a3595
445476 <babay445476@gmail.com>
2025-05-19 00:18:10 +0300

    Made homework

---
+++ Homework/Homework/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class Matrix
+    {
+        private char[,] matrix;
+        private int digitCount;
+
+        public Matrix(char[,] inputMatrix)
+        {
+            matrix = inputMatrix;
+            digitCount = Count();
+        }
+
+        public string this[int column]
+        {
+            get
+            {
+                string result = "";          
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    result += matrix[row, column];
+                }
+                return result;
+            }
+        }
+
+        public int DigitCount
+        {
+            get { return digitCount; }
+        }
+
+        private int Count()
+        {
+            int count = 0;
+            foreach (char c in matrix)
+            {
+                if (char.IsDigit(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Homework/Homework/Program.cs
@@ -0,0 +1,17 @@
+﻿using Homework;
+using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Matrix matrix = new Matrix(new char[,] {
+            { '1', 'a', 'a' },
+            { '3', 'b', 'b' },
+            { 'c', '4', 'c' }
+        });
+
+        Console.WriteLine(matrix[1]);
+        Console.WriteLine(matrix.DigitCount);
+    }
+}
